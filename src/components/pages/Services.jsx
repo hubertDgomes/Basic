bfea47a293153b18501948003f8264559105d2bc
Hubert Dhrubo Gomes <gomeshubert8@gmail.com>
2025-08-23 11:33:14 +0600

    Add lenis library; update Services and About pages with new content and layout

--- src/components/pages/Services.jsx
+++ src/components/pages/Services.jsx
@@ -1,11 +1,117 @@
-import React from 'react'
+import React from "react";
+import ScrollStack, { ScrollStackItem } from "/src/components/ScrollStack";
 
 const Services = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <ScrollStack>
+        <h1 className="text-5xl text-center font-Karla">I will Do</h1>
+        <ScrollStackItem>
+          <h2 className="text-center text-[40px] font-Karla font-bold">
+            Web Design
+          </h2>
+          <p className="font-Inter text-[17px] mt-[20px]">
+            Hubert is a leading web design agency with an award-winning design
+            team that creates innovative, effective websites that capture your
+            brand, improve your conversion rates, and maximize your revenue to
+            help grow your business and achieve your goals.
+          </p>
+          <p className="font-Inter text-[17px] mt-[20px]">
+            In today’s digital world, your website is the first interaction
+            consumers have with your business. That's why almost 95 percent of a
+            user’s first impression relates to web design. It’s also why web
+            design services can have an immense impact on your company’s bottom
+            line.
+          </p>
+          <p className="font-Inter text-[17px] mt-[20px]">
+            That’s why more companies are not only reevaluating their website’s
+            design but also partnering with Hubert, the web design agency that’s
+            driven more than $2.4 billion in revenue for its clients. With over
+            50 web design awards under our belt, we're confident we can design a
+            custom website that drives sales for your unique business.
+          </p>
+        </ScrollStackItem>
 
-export default Services
+        <ScrollStackItem>
+          <h2 className="text-center text-[40px] font-Karla font-bold">
+            Front-End
+          </h2>
+          <p className="font-Inter text-[17px] mt-[20px]">
+            Hubert is a leading web design agency with an award-winning design
+            team that creates innovative, effective websites that capture your
+            brand, improve your conversion rates, and maximize your revenue to
+            help grow your business and achieve your goals.
+          </p>
+          <p className="font-Inter text-[17px] mt-[20px]">
+            In today’s digital world, your website is the first interaction
+            consumers have with your business. That's why almost 95 percent of a
+            user’s first impression relates to web design. It’s also why web
+            design services can have an immense impact on your company’s bottom
+            line.
+          </p>
+          <p className="font-Inter text-[17px] mt-[20px]">
+            That’s why more companies are not only reevaluating their website’s
+            design but also partnering with Hubert, the web design agency that’s
+            driven more than $2.4 billion in revenue for its clients. With over
+            50 web design awards under our belt, we're confident we can design a
+            custom website that drives sales for your unique business.
+          </p>
+        </ScrollStackItem>
+
+        <ScrollStackItem>
+          <h2 className="text-center text-[40px] font-Karla font-bold">
+            Figma to React JS
+          </h2>
+          <p className="font-Inter text-[17px] mt-[20px]">
+            Hubert is a leading web design agency with an award-winning design
+            team that creates innovative, effective websites that capture your
+            brand, improve your conversion rates, and maximize your revenue to
+            help grow your business and achieve your goals.
+          </p>
+          <p className="font-Inter text-[17px] mt-[20px]">
+            In today’s digital world, your website is the first interaction
+            consumers have with your business. That's why almost 95 percent of a
+            user’s first impression relates to web design. It’s also why web
+            design services can have an immense impact on your company’s bottom
+            line.
+          </p>
+          <p className="font-Inter text-[17px] mt-[20px]">
+            That’s why more companies are not only reevaluating their website’s
+            design but also partnering with Hubert, the web design agency that’s
+            driven more than $2.4 billion in revenue for its clients. With over
+            50 web design awards under our belt, we're confident we can design a
+            custom website that drives sales for your unique business.
+          </p>
+        </ScrollStackItem>
+
+        <ScrollStackItem>
+          <h2 className="text-center text-[40px] font-Karla font-bold">
+            Responsive Website
+          </h2>
+          <p className="font-Inter text-[17px] mt-[20px]">
+            Hubert is a leading web design agency with an award-winning design
+            team that creates innovative, effective websites that capture your
+            brand, improve your conversion rates, and maximize your revenue to
+            help grow your business and achieve your goals.
+          </p>
+          <p className="font-Inter text-[17px] mt-[20px]">
+            In today’s digital world, your website is the first interaction
+            consumers have with your business. That's why almost 95 percent of a
+            user’s first impression relates to web design. It’s also why web
+            design services can have an immense impact on your company’s bottom
+            line.
+          </p>
+          <p className="font-Inter text-[17px] mt-[20px]">
+            That’s why more companies are not only reevaluating their website’s
+            design but also partnering with Hubert, the web design agency that’s
+            driven more than $2.4 billion in revenue for its clients. With over
+            50 web design awards under our belt, we're confident we can design a
+            custom website that drives sales for your unique business.
+          </p>
+        </ScrollStackItem>
+      </ScrollStack>
+    </>
+  );
+};
+
+export default Services;
