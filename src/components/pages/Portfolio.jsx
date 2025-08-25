2eb7c0d2599a376c1b166a96e415be28ae268ddf
Hubert Dhrubo Gomes <gomeshubert8@gmail.com>
2025-08-25 23:30:20 +0600

    Refactor code structure for improved readability and maintainability

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -1,40 +1,92 @@
-import React from 'react'
-import Container from '../Container'
+import React from "react";
+import Container from "../Container";
+import Images from "../Images";
+import show1 from "/src/assets/show1.png";
+import show2 from "/src/assets/show2.png";
+import show3 from "/src/assets/show3.png";
+import show4 from "/src/assets/show4.png";
+import show5 from "/src/assets/show5.png";
+import show6 from "/src/assets/show6.png";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const Portfolio = () => {
-  const ServiceBar = ({no,task,taskInfo})=>{
-    return(
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  };
+
+  const PortfolioItems = ({ Show, PageName, link }) => {
+    return (
       <>
-      <div className="py-[40px] px-[40px] border-1 border-black w-[380px] hover:scale-110 transition-all duration-500 cursor-pointer">
-        <p className='font-Inter text-[20px] font-semibold text-[#130F4980]'>{no}</p>
-        <p className='font-Inter text-[20px] font-bold text-[#130F49]'>{task}</p>
-        <p className='font-Karla text-[18px] font-light text-[#55527C]'>{taskInfo}</p>
-        <p>Read More</p>
-      </div>
+        <div className="mx-[30px]">
+          <a href={link} target="_blank">
+          <div className="p-2 ">
+            <Images
+              src={Show}
+              className={
+                "w-[700px] h-[250px] p-5 scale-100 hover:scale-110 transition-all duration-500 cursor-pointer"
+              }
+            />
+            <p className="text-center mt-[10px] font-Karla font-bold text-[#130F49] text-[20px]">
+              {PageName}
+            </p>
+          </div>
+        </a>
+        </div>
       </>
-    )
-  }
+    );
+  };
+
   return (
     <Container className={""}>
       <div className="h-screen flex flex-col items-center justify-center">
         <div className="flex justify-between w-full items-baseline-last">
-          <div className="">
-            <h1 className='font-Karla text-[#55527C] text-[18px] font-semibold'>-SERVICES</h1>
-            <h1 className='text-[40px] font-Inter text-[#130F49] font-bold'>MY Services</h1>
+          <div className="mb-[30px]">
+            <h1 className="font-Karla text-[#55527C] text-[18px] font-semibold">
+              -PROJECTS
+            </h1>
+            <h1 className="text-[40px] font-Inter text-[#130F49] font-bold">
+              Recent completed works
+            </h1>
           </div>
           <div className="">
-            <p className='font-Inter text-[20px] text-[#55527C] font-bold underline'>gomeshubert8@gmail.com</p>
+            <p className="font-Inter text-[20px] text-[#55527C] font-bold underline">
+              gomeshubert8@gmail.com
+            </p>
           </div>
         </div>
-        <div className="flex justify-between flex-wrap gap-y-[40px]">
-          <ServiceBar no={"01"} task={"Web Design"} taskInfo={"Web Design is the process of building programing etc blah"}/>
-          <ServiceBar no={"01"} task={"Web Design"} taskInfo={"Web Design is the process of building programing etc blah"}/>
-          <ServiceBar no={"01"} task={"Web Design"} taskInfo={"Web Design is the process of building programing etc blah"}/>
-          <ServiceBar no={"01"} task={"Web Design"} taskInfo={"Web Design is the process of building programing etc blah"}/>
+
+        <div className="w-full overflow-hidden">
+          <Slider {...settings}>
+          <div>
+            <PortfolioItems link={"https://oribi-mu.vercel.app/"} Show={show1} PageName={"Orebi"}/>
+          </div>
+          <div>
+            <PortfolioItems link={"https://hancok-three.vercel.app/"} Show={show2} PageName={"Hankok"}/>
+          </div>
+          <div>
+            <PortfolioItems link={"https://uomo-beta.vercel.app/"} Show={show3} PageName={"UOMO"}/>
+          </div>
+          <div>
+            <PortfolioItems link={"https://hubertdgomes.github.io/furino2/"} Show={show4} PageName={"Furnio"}/>
+          </div>
+          <div>
+            <PortfolioItems link={"https://flyver.vercel.app/"} Show={show5} PageName={"Flyver"}/>
+          </div>
+          <div>
+            <PortfolioItems link={"https://hubertdgomes.github.io/TechEdge/"} Show={show6} PageName={"Techedge"}/>
+          </div>
+        </Slider>
         </div>
+        
       </div>
     </Container>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
